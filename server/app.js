07d0c90d9fac98ecd580e9c1ac7031b8a30410c5
josue rojas <josuerojas.rojas@gmail.com>
2019-07-13 16:56:22 -0400

    added dummy data and todo functions

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var reservationsRouter = require('./routes/reservations');
@@ -17,6 +18,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// app.use(bodyParser);
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- server/routes/reservations.js
+++ server/routes/reservations.js
@@ -1,11 +1,70 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json([{id: 1, name: 'Zvjezdana Birthe, party of 2 @8pm'},
-    { id: 2, name: 'Zydrunas Ilgauskas, party of 3 @3pm' }]);
+const dummyText = {
+  ToCountry: 'US',
+  ToState: 'NY',
+  SmsMessageSid: 'SM7cced361b3456680298a175fe930bfaf',
+  NumMedia: '0',
+  ToCity: 'NEW YORK',
+  FromZip: '11201',
+  SmsSid: 'SM7cced361b3456680298a175fe930bfaf',
+  FromState: 'NY',
+  SmsStatus: 'received',
+  FromCity: 'BROOKLYN',
+  Body: 'Reservation for Josue Rojas at July 20, 2019 at 8pm.',
+  FromCountry: 'US',
+  To: '+13479707025',
+  ToZip: '10014',
+  NumSegments: '1',
+  MessageSid: 'SM7cced361b3456680298a175fe930bfaf',
+  AccountSid: 'AC4c12235c6778228c9299f358d55e2071',
+  From: '+17182197093',
+  ApiVersion: '2010-04-01'
+}
 
+const dummyJson = [
+  {
+    name: 'Zvjezdana Birthe',
+    dateTime: 'July 20, 2019 03:24:00',
+    phoneNumber: '+19374165537',
+    createdAt: 'July 13, 2019 16:54:00',
+    rawJson: '',
+    restaurantId: '1'
+  },
+  {
+    name: 'Miro Njeri',
+    dateTime: 'July 30, 2019 18:04:00',
+    phoneNumber: '+18155701572',
+    createdAt: 'July 13, 2019 16:54:00',
+    rawJson: '',
+    restaurantId: '1'
+  },
+  {
+    name: 'Justine Feige',
+    dateTime: 'August 3, 2019 8:30:00',
+    phoneNumber: '+12565355323',
+    createdAt: 'July 13, 2019 16:54:00',
+    rawJson: '',
+    restaurantId: '1'
+  },
+  {
+    name: 'Tao Kylee',
+    dateTime: 'August 17, 2019 10:30:00',
+    phoneNumber: '+19527072487',
+    createdAt: 'July 13, 2019 16:54:00',
+    rawJson: '',
+    restaurantId: '1'
+  }
+];
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  res.json(dummyJson);
+});
+
+router.post('/', (req, res, next) => {
+  res.json(req.body);
 });
 
 module.exports = router;
